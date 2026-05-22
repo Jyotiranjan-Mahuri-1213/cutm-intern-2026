b649b84c565f3efca8c717c4b2556f3ca991fcfb
Jyotiranjan-Mahuri-1213 <jyotiranjanmahuri@gmail.com>
2026-05-22 23:11:42 +0530

    Initial commit

---
+++ declare_display.java
@@ -0,0 +1,15 @@
+/*Write a program to declare variables of different data types and display their values.*/
+
+public class declare_display{
+    public static void main (String []args){
+        int a=5;
+        char ch='J';
+        double pi=3.14;
+        String subject="java";
+        System.out.println("values are : ");
+        System.out.println(a);
+        System.out.println(ch);
+        System.out.println(subject);
+        System.out.println(pi);
+    }
+}
