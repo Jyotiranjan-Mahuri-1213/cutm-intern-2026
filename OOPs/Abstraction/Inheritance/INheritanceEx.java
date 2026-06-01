115f3562e39824830a9937328f1473b14cc7c51d
Jyotiranjan-Mahuri-1213 <jyotiranjanmahuri@gmail.com>
2026-06-01 23:59:28 +0530

    OOPs concept

---
+++ OOPs/Abstraction/Inheritance/INheritanceEx.java
@@ -0,0 +1,21 @@
+package OOPs.Abstraction.Inheritance;
+
+class Sim {
+    void connect() {
+        System.out.println("Connecting to network...");
+    }
+}
+
+class Jio extends Sim {
+    void activate5G() {
+        System.out.println("Jio 5G features activated.");
+    }
+}
+public class INheritanceEx {
+
+    public static void main(String[] args) {
+        Jio myJio = new Jio();
+        myJio.connect();
+        myJio.activate5G();
+    }
+}
